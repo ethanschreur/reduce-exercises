52920265ae400c5058d7266976c0dcc8b3e6db7b
ethanschreur <ethanschreur@icloud.com>
2021-01-13 00:21:58 -0500

    done

--- reduce.js
+++ reduce.js
@@ -2,11 +2,18 @@
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
 Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+    
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+	return arr.reduce(function(accumulator, nextValue) {
+		accumulator.push(nextValue[key]);
+		return accumulator;
+	}, []);
+}
+const arr = [ { name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' } ];
+console.log(extractValue(arr, 'name'));
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,8 +26,25 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
-
+function vowelCount(str) {
+	return str.split('').reduce(function(accumulator, nextValue) {
+		nextValue = nextValue.toLowerCase();
+		let nextValueIsVowel = 'aeiou'.includes(nextValue);
+		if (nextValueIsVowel) {
+			if (accumulator[nextValue]) {
+				accumulator[nextValue]++;
+			} else {
+				accumulator[nextValue] = 1;
+			}
+		}
+		return accumulator;
+	}, {});
+}
+console.log(vowelCount('Elie')); // {e:2,i:1};
+console.log(vowelCount('Tim')); // {i:1};
+console.log(vowelCount('Matt')); // {a:1})
+console.log(vowelCount('hmmm')); // {};
+console.log(vowelCount('I Am awesome and so are you')); // {i: 1, a: 4, e: 3, o: 3, u: 1};
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
@@ -36,7 +60,16 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+	return arr.reduce(function(accumulator, nextValue) {
+		nextValue[key] = value;
+		accumulator.push(nextValue);
+		return accumulator;
+	}, []);
+}
+const arr2 = [ { name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' } ];
+
+console.log(addKeyAndValue(arr2, 'title', 'Instructor')); //
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -60,4 +93,28 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+	return arr.reduce(
+		function(accumulator, nextValue) {
+			callback(nextValue) ? accumulator[0].push(nextValue) : accumulator[1].push(nextValue);
+			return accumulator;
+		},
+		[ [], [] ]
+	);
+}
+
+function isEven(val) {
+	return val % 2 === 0;
+}
+
+const arr3 = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+console.log(partition(arr3, isEven)); // [[2,4,6,8], [1,3,5,7]];
+
+function isLongerThanThreeCharacters(val) {
+	return val.length > 3;
+}
+
+const names = [ 'Elie', 'Colt', 'Tim', 'Matt' ];
+
+console.log(partition(names, isLongerThanThreeCharacters)); // [['Elie', 'Colt', 'Matt'], ['Tim']]
